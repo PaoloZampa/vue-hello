7e7f906c41e5a06bf5c11a2db8c59395125e205b
PaoloZampa <nlmp.paolozampa@gmail.com>
2023-03-28 15:59:41 +0200

    initial commit add Hello vue h1

---
+++ main.js
@@ -0,0 +1,16 @@
+ /* Stampare a schermo un messaggio all’interno di un h1  utilizzando una proprietá nell'oggetto restituito dalla funzione data.
+
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+Buon divertimento e confermate lettura come al solito! */
+ 
+ 
+ const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        myMessage: 'Hello Vueeeeee!'
+      }
+    }
+  }).mount('#app')
